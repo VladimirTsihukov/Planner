f6afa00725bf0eb96b1b5a14d1374348dc48f0d9
vladimir.tishukov <vladimir.tishukov@onetwotrip.com>
2025-03-23 00:15:36 +0100

    ios connections

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    alias(libs.plugins.compose) apply false
     alias(libs.plugins.multiplatform) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.android.application) apply false
---
+++ iosApp/iosApp/iosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosAppApp.swift
+//  iosApp
+//
+//  Created by Vladimir Tishukov on 22.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct iosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
